c1c38a4d98c272680e4bec87fa933f10530d8bd6
ctrujillo <trujillogarciac2@gmail.com>
2021-03-04 12:20:13 -0500

    Se Terminan Los Ejercicios

--- main.js
+++ main.js
@@ -42,3 +42,55 @@ function ejercicioCuatro () {
 
     document.getElementById("Resultado").value = parseFloat(pepito)+parseFloat(jose)+parseFloat(Carlos);
 }
+
+function ejercicioCinco (accion = '0') {
+    var pepito1         = document.getElementById("pepito5").value;
+    var anhoNacimiento  = 2021 - pepito1;
+
+    if (accion == 0) {
+        document.getElementById("resultado5").value = anhoNacimiento;
+    } else if (accion == 1) {
+        alert("El año de nacimiento es: "+anhoNacimiento);
+    }
+
+}
+
+function ejercicioSeis (accion = 0) {
+    var altura  = document.getElementById("altura6").value;
+    var base    = document.getElementById("base6").value;
+
+    var resultado = base * altura;
+
+    if (accion == 0) {
+        document.getElementById("resultado6").value = resultado;
+    } else if (accion == 1) {
+        alert("El Area del rectangulo es de: "+resultado);
+    }
+
+}
+
+function ejercicioSiete (accion = '0') {
+    var dinero  = document.getElementById("dinero7").value;
+    var entrada = document.getElementById("entrada7").value;
+
+    var resultado = dinero - entrada;
+
+    if (accion == 0) {
+        document.getElementById("resultado7").value = resultado;
+    } else if (accion == 1) {
+        alert("El dinero restante para entrar a cine es: $ "+resultado);
+    }
+}
+
+function ejercicioOcho (accion = '0') {
+    var dinero  = document.getElementById("dinero8").value;
+    var entrada = document.getElementById("entrada8").value;
+
+    var resultado = dinero / entrada;
+
+    if (accion == 0) {
+        document.getElementById("resultado8").value = parseInt(resultado);
+    } else if (accion == 1) {
+        alert("Puedes invitar a "+parseInt(resultado)+" persona(s) al estadio.");
+    }
+}

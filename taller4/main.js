3eae4abe7f4f12da2dd08472072af9281d23ef7a
ctrujillo <trujillogarciac2@gmail.com>
2021-03-12 20:08:38 -0500

    Ejercicio 4

--- taller4/main.js
+++ taller4/main.js
@@ -15,27 +15,12 @@ function ejercicioUno () {
 }
 
 function ejercicioDos () {
-    //Leer 10 enteros, almacenarlos en un vector y determinar en qué posición del vector está el mayor número par leído.
+    var numeroUno = prompt("Dame un primer número");
 
-    var numeros = [];
+    var potenciaDos     = Math.pow(numeroUno, 2);
+    var potenciaTres    = Math.pow(numeroUno, 3);
 
-    for(let i=0; i<10; i++){
-        numeros[i] = parseInt(Math.random() * (100 - 0) + 0);
-    }
-
-    console.log(numeros);
-
-    var numeroMayor = 0;
-    var posicionMayor = 0
-
-    for (let i = 0; i < numeros.length; i++) {
-        if (numeros[i] > numeroMayor && numeros[i]%2 == 0) {
-            numeroMayor = numeros[i];
-            posicionMayor = i + 1;
-        }
-    }
-
-    console.log("Su número mayor es: "+numeroMayor+" y su posición es: "+posicionMayor)
+    alert("La potencia cuadrada es: "+potenciaDos+" y su potencia cubica: "+potenciaTres);
 }
 
 function ejercicioTres () {
